1a56bdf0b400177cd31a5f8797defaec08e70780
DorotaBiela <bilkapl@gmail.com>
2022-08-23 17:55:31 +0200

    Add StartupRunner

---
+++ src/main/java/pl/sdacademy/projektplus/quiz/StartupRunner.java
@@ -0,0 +1,18 @@
+package pl.sdacademy.projektplus.quiz;
+
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import pl.sdacademy.projektplus.quiz.database.entities.PlayerEntity;
+
+@Component
+@Log
+public class StartupRunner implements CommandLineRunner {
+    @Override
+    public void run(String...args) throws Exception {
+        log.info("Executing startup actions...");
+
+        PlayerEntity player = new PlayerEntity("Dorota");
+        log.info("Created player: " + player);
+    }
+}
